61a8b11e9e206ab188345c6c93e121944335def2
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2022-01-11 11:33:13 +0100

    Moved to typescript and parcel

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-var canvas = document.getElementById("canvas");
-width = window.innerWidth * 5;
-height = window.innerHeight * 3;
+var canvas = <HTMLCanvasElement>document.getElementById("canvas");
+const width = window.innerWidth * 5;
+const height = window.innerHeight * 3;
+const cellSize = 4;
+// @ts-ignore:
 canvas.width = width;
+// @ts-ignore:
 canvas.height = height;
-var ctx = canvas.getContext("2d");
-const cellSize = 5;
-// Create a grid the size of the window/cellSize
-const grid = Array(parseInt(height / cellSize))
-  .fill()
-  .map(() => Array(parseInt(width / cellSize)).fill(false));
-console.log(grid);
-function rule(row) {
+const ctx = canvas.getContext("2d");
+
+const grid = Array(Math.floor(height / cellSize))
+  .fill(null)
+  .map(() => Array(Math.floor(width / cellSize)).fill(false));
+
+function rule(row: boolean[]) {
   // Rule 30
   const rules = {
-    111: false,
-    110: false,
-    101: false,
-    100: true,
+    "111": false,
+    "110": false,
+    "101": false,
+    "100": true,
     "011": true,
     "010": true,
     "001": true,
@@ -36,15 +38,17 @@ function rule(row) {
   // }
   const arr = Array(row.length).fill(false);
   for (let i = 1; i < row.length - 1; i++) {
-    toString = row
-      .slice(i - 1, i + 2)
-      .map((x) => (x ? "1" : "0"))
-      .join("");
-    arr[i] = rules[toString];
+    arr[i] =
+      rules[
+        row
+          .slice(i - 1, i + 2)
+          .map((x) => (x ? "1" : "0"))
+          .join("")
+      ];
   }
   return arr;
 }
-grid[0][parseInt(grid[0].length / 2)] = true;
+grid[0][Math.floor(grid[0].length / 2)] = true;
 for (let y = 0; y < grid.length - 1; y++) {
   for (let x = 0; x < grid[y].length; x++) {
     if (grid[y][x])
