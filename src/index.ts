d444ff07fa258b93eb81e5d63396bcefccece919
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2022-01-12 14:45:44 +0100

    Added more rules and refactored code

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,72 @@
+import { rule18, rule86 } from "./rules";
+
+const { rule30, rule110 } = require("./rules");
 var canvas = <HTMLCanvasElement>document.getElementById("canvas");
 const width = window.innerWidth * 5;
 const height = window.innerHeight * 3;
 const cellSize = 4;
-// @ts-ignore:
-canvas.width = width;
-// @ts-ignore:
-canvas.height = height;
 const ctx = canvas.getContext("2d");
 
-const grid = Array(Math.floor(height / cellSize))
-  .fill(null)
-  .map(() => Array(Math.floor(width / cellSize)).fill(false));
+// @ts-ignore
+canvas.width = width;
+// @ts-ignore
+canvas.height = height;
 
-function rule(row: boolean[]) {
-  // Rule 30
-  const rules = {
-    "111": false,
-    "110": false,
-    "101": false,
-    "100": true,
-    "011": true,
-    "010": true,
-    "001": true,
-    "000": false,
-  };
+interface ruleInterface {
+  "111": boolean;
+  "110": boolean;
+  "101": boolean;
+  "100": boolean;
+  "011": boolean;
+  "010": boolean;
+  "001": boolean;
+  "000": boolean;
+}
 
-  // Rule 110
-  // const rules = {
-  //   "111": false,
-  //   "110": true,
-  //   "101": true,
-  //   "100": false,
-  //   "011": true,
-  //   "010": true,
-  //   "001": true,
-  //   "000": false
-  // }
-  const arr = Array(row.length).fill(false);
-  for (let i = 1; i < row.length - 1; i++) {
-    arr[i] =
-      rules[
-        row
-          .slice(i - 1, i + 2)
-          .map((x) => (x ? "1" : "0"))
-          .join("")
-      ];
+const buttons = {
+  Rule18: rule18,
+  Rule30: rule30,
+  Rule110: rule110,
+  Rule86: rule86,
+};
+
+Object.entries(buttons).forEach(([key, value]) => {
+  const button = document.createElement("button");
+  button.innerHTML = "Rule " + key.slice(4);
+  button.addEventListener("click", () => {
+    renderCA(value);
+  });
+  document.getElementById("ruleButtonContainer").appendChild(button);
+});
+
+function renderCA(rule: ruleInterface) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const grid = Array(Math.floor(height / cellSize))
+    .fill(null)
+    .map(() => Array(Math.floor(width / cellSize)).fill(false));
+
+  function applyRule(row: boolean[], rules: ruleInterface) {
+    // Rule 110
+    const arr = Array(row.length).fill(false);
+    for (let i = 1; i < row.length - 1; i++) {
+      arr[i] =
+        rules[
+          row
+            .slice(i - 1, i + 2)
+            .map((x) => (x ? "1" : "0"))
+            .join("")
+        ];
+    }
+    return arr;
   }
-  return arr;
-}
-grid[0][Math.floor(grid[0].length / 2)] = true;
-for (let y = 0; y < grid.length - 1; y++) {
-  for (let x = 0; x < grid[y].length; x++) {
-    if (grid[y][x])
-      ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+  grid[0][Math.floor(grid[0].length / 2)] = true;
+  for (let y = 0; y < grid.length - 1; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      if (grid[y][x])
+        ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+    grid[y + 1] = applyRule(grid[y], rule);
   }
-  grid[y + 1] = rule(grid[y]);
 }
+
+renderCA(rule30);
---
+++ src/rules.ts
@@ -0,0 +1,43 @@
+export const rule18 = {
+  "111": false,
+  "110": false,
+  "101": false,
+  "100": true,
+  "011": false,
+  "010": false,
+  "001": true,
+  "000": false,
+};
+
+export const rule30 = {
+  "111": false,
+  "110": false,
+  "101": false,
+  "100": true,
+  "011": true,
+  "010": true,
+  "001": true,
+  "000": false,
+};
+
+export const rule86 = {
+  "111": false,
+  "110": true,
+  "101": false,
+  "100": true,
+  "011": false,
+  "010": true,
+  "001": true,
+  "000": false,
+};
+
+export const rule110 = {
+  "111": false,
+  "110": true,
+  "101": true,
+  "100": false,
+  "011": true,
+  "010": true,
+  "001": true,
+  "000": false,
+};
