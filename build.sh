e44c5e6ed88728ab4dcdf9f489ac9d6491b3aa55
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2023-03-09 21:09:58 +0100

    Building everything into html

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 rm employment-diagram.svg
 mmdc -i employment-diagram.md -o employment-diagram.svg -t forest -b transparent
+curl "https://github-readme-stats.vercel.app/api/top-langs/?username=ridulfo&langs_count=8&exclude_repo=blog" > stats.svg
