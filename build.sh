4442314fda139657e70c04fb90f950dc9e68f7db
Nicolo Ridulfo <nicoloridulfo@gmail.com>
2023-03-18 14:45:25 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-rm employment-diagram.svg
-mmdc -i employment-diagram.md -o employment-diagram.svg -t forest -b transparent
-curl "https://github-readme-stats.vercel.app/api/top-langs/?username=ridulfo&langs_count=8&exclude_repo=blog" > stats.svg
+rm public/employment-diagram.svg
+mmdc -i employment-diagram.md -o public/employment-diagram.svg -t forest -b transparent
+mv public/employment-diagram-1.svg public/employment-diagram.svg
+curl "https://github-readme-stats.vercel.app/api/top-langs/?username=ridulfo&langs_count=8&exclude_repo=blog&hide=Jupyter%20Notebook&bg_color=00000000&theme=transparent" > public/stats.svg
