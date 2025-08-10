c78494d9f08575b092a18f888ddf29b047968f76
Nicolo <nicoloridulfo@gmail.com>
2025-08-10 19:30:12 +0200

    clean up

--- build.sh
+++
@@ -1,4 +0,0 @@
-rm public/employment-diagram.svg
-yarn mmdc -i employment-diagram.md -o public/employment-diagram.svg -t forest -b transparent
-mv public/employment-diagram-1.svg public/employment-diagram.svg
-curl "https://github-readme-stats.vercel.app/api/top-langs/?username=ridulfo&langs_count=8&exclude_repo=blog,comodoro&hide=Jupyter%20Notebook&bg_color=00000000&theme=transparent" > public/stats.svg
